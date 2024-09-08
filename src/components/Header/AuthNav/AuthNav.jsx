de6c1c7418d3224c5883d9ebb11db883e770d104
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-09-08 17:14:49 +0900

    fix errors

--- src/components/Header/AuthNav/AuthNav.jsx
+++ src/components/Header/AuthNav/AuthNav.jsx
@@ -1,133 +1,126 @@
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { IoIosArrowDown } from 'react-icons/io';
-import { HiOutlineUserCircle } from 'react-icons/hi2';
-import css from './AuthNav.module.css';
-import Logo from '../../../images/logo.png';
-import { routes } from 'routes/routes';
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+import { IoIosArrowDown } from "react-icons/io";
+import { HiOutlineUserCircle } from "react-icons/hi2";
+import css from "./AuthNav.module.css";
+import Logo from "../../../images/logo.png";
+import { routes } from "routes/routes";
 
-import LogOut from '../LogOut/Logout';
-import ModalSetting from '../ModalSetting/ModalSetting';
-import ModalUser from '../ModalUser/ModalUser';
-import { selectIsLoggedIn } from '../../../redux/auth/auth.selectors';
-import { selectUser } from '../../../redux/auth/auth.selectors';
+import LogOut from "../LogOut/Logout";
+import ModalSetting from "../ModalSetting/ModalSetting";
+import ModalUser from "../ModalUser/ModalUser";
+import { selectIsLoggedIn } from "../../../redux/auth/auth.selectors";
+import { selectUser } from "../../../redux/auth/auth.selectors";
 
 export default function AuthNav() {
-    const isLoggedIn = useSelector(selectIsLoggedIn);
-    const userData = useSelector(selectUser);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const userData = useSelector(selectUser);
 
-    const name = userData?.name || '';
-    const email = userData?.email || '';
-    const photo = userData?.photo || null;
+  const name = userData?.name || "";
+  const email = userData?.email || "";
+  const photo = userData?.photo || null;
 
-    //=========modalOpenSetting===================
-    const [modalIsOpen, setIsOpen] = useState(false);
-    function openModal() {
-        setIsOpen(true);
-    }
-    function closeModal() {
-        setIsOpen(false);
-    }
-    //=========ContextModal=======================
-    const [contextModalIsOpen, setIsContextModalOpen] = useState(false);
-    function openContext() {
-        setIsContextModalOpen(true);
-    }
-    function closeContext() {
-        setIsContextModalOpen(false);
-    }
-    //=========LogOutModal========================
-    const [logOutModalIsOpen, setLogOutModalIsOpen] = useState(false);
-    function openLogOut() {
-        setLogOutModalIsOpen(true);
-    }
-    function closeLogOut() {
-        setLogOutModalIsOpen(false);
-    }
-    const handleClick = () => {
-        openContext();
-    };
+  //=========modalOpenSetting===================
+  const [modalIsOpen, setIsOpen] = useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
+  //=========ContextModal=======================
+  const [contextModalIsOpen, setIsContextModalOpen] = useState(false);
+  function openContext() {
+    setIsContextModalOpen(true);
+  }
+  function closeContext() {
+    setIsContextModalOpen(false);
+  }
+  //=========LogOutModal========================
+  const [logOutModalIsOpen, setLogOutModalIsOpen] = useState(false);
+  function openLogOut() {
+    setLogOutModalIsOpen(true);
+  }
+  function closeLogOut() {
+    setLogOutModalIsOpen(false);
+  }
+  const handleClick = () => {
+    openContext();
+  };
 
-    const imageDefault = () => {
-        if (photo) {
-            return photo;
-        } else if (name) {
-            return name.slice(0, 1).toUpperCase();
-        }
-        return email.slice(0, 1).toUpperCase();
-    };
-    //==================================
-    const navigate = useNavigate();
-    const handleSignInClick = () => {
-        navigate(routes.LOGIN);
-    };
-    return (
-        <div className={css.wrap}>
-            {isLoggedIn ? (
-                <NavLink to={routes.HOMEPAGE}>
-                    {/* Home Page */}
-                    <img src={Logo} width="102" height="48" alt="Logo" />
-                </NavLink>
-            ) : (
-                <NavLink to={routes.WELCOMEPAGE}>
-                    {/* Welcom Page */}
-                    <img src={Logo} width="102" height="48" alt="Logo" />
-                </NavLink>
-            )}
-            {isLoggedIn ? (
-                <div className={css['user-container']}>
-                    <p className={css['name-text']}>{name}</p>
-                    {!photo ? (
-                        <p className={css['first-letter']}>{imageDefault()}</p>
-                    ) : (
-                        <img
-                            src={imageDefault()}
-                            className={css.image}
-                            width="28"
-                            height="28"
-                            alt="user"
-                        />
-                    )}
-                    <button
-                        type="button"
-                        className={css.button}
-                        onClick={handleClick}
-                    >
-                        <IoIosArrowDown />
-                    </button>
-                </div>
-            ) : (
-                <div className={css['user-container']}>
-                    <button
-                        type="button"
-                        className={css['button-sin']}
-                        onClick={handleSignInClick}
-                    >
-                        Sign in
-                        <HiOutlineUserCircle className={css.icon} />
-                    </button>
-                </div>
-            )}
-            <ModalUser
-                openLogOut={openLogOut}
-                openModal={openModal}
-                closeContext={closeContext}
-                contextModalIsOpen={contextModalIsOpen}
+  const imageDefault = () => {
+    if (photo) {
+      return photo;
+    } else if (name) {
+      return name.slice(0, 1).toUpperCase();
+    }
+    return email.slice(0, 1).toUpperCase();
+  };
+  //==================================
+  const navigate = useNavigate();
+  const handleSignInClick = () => {
+    navigate(routes.LOGIN);
+  };
+  return (
+    <div className={css.wrap}>
+      {isLoggedIn ? (
+        <NavLink to={routes.HOMEPAGE}>
+          {/* Home Page */}
+          <img src={Logo} width="102" height="48" alt="Logo" />
+        </NavLink>
+      ) : (
+        <NavLink to={routes.WELCOMEPAGE}>
+          {/* Welcom Page */}
+          <img src={Logo} width="102" height="48" alt="Logo" />
+        </NavLink>
+      )}
+      {isLoggedIn ? (
+        <div className={css["user-container"]}>
+          <p className={css["name-text"]}>{name}</p>
+          {!photo ? (
+            <p className={css["first-letter"]}>{imageDefault()}</p>
+          ) : (
+            <img
+              src={imageDefault()}
+              className={css.image}
+              width="28"
+              height="28"
+              alt="user"
             />
-            <LogOut
-                logOutModalIsOpen={logOutModalIsOpen}
-                closeLogOut={closeLogOut}
-            />
-            {email ? (
-                <ModalSetting
-                    closeModal={closeModal}
-                    isOpen={modalIsOpen}
-                    user={userData}
-                />
-            ) : null}
+          )}
+          <button type="button" className={css.button} onClick={handleClick}>
+            <IoIosArrowDown />
+          </button>
         </div>
-    );
+      ) : (
+        <div className={css["user-container"]}>
+          <button
+            type="button"
+            className={css["button-sin"]}
+            onClick={handleSignInClick}
+          >
+            Sign in
+            <HiOutlineUserCircle className={css.icon} />
+          </button>
+        </div>
+      )}
+      <ModalUser
+        openLogOut={openLogOut}
+        openModal={openModal}
+        closeContext={closeContext}
+        contextModalIsOpen={contextModalIsOpen}
+      />
+      <LogOut logOutModalIsOpen={logOutModalIsOpen} closeLogOut={closeLogOut} />
+      {email ? (
+        <ModalSetting
+          closeModal={closeModal}
+          isOpen={modalIsOpen}
+          user={userData}
+        />
+      ) : null}
+    </div>
+  );
 }
 // andrii@gmail.com
 // 1111111111
