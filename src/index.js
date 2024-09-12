a3eed2c786a1a3f0e6eeeba552b75d18ed35103c
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-09-12 21:11:27 +0900

    update index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ if (rootElement) {
       {" "}
       {/* Wrap App with Provider */}
       <PersistGate loading={null} persistor={persistor}>
-        <Router>
+        <Router basename="/team-project-03/">
           {" "}
           {/* Wrap with Router and set basename */}
           <App />
