8fd81f5a1dd6ac5e385be8f23ab995277674a99a
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-09-12 21:05:54 +0900

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux'; // Import Provider
-import { store, persistor } from './redux/store.js'; // Import your Redux store
-import App from './components/App'; // Make sure the import path is correct
-import './index.css';
-import 'modern-normalize';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux"; // Import Provider
+import { store, persistor } from "./redux/store.js"; // Import your Redux store
+import App from "./components/App"; // Make sure the import path is correct
+import "./index.css";
+import "modern-normalize";
+import { BrowserRouter as Router } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 // Get the root element
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 if (rootElement) {
-    // Create a root using ReactDOM.createRoot
-    const root = ReactDOM.createRoot(rootElement);
+  // Create a root using ReactDOM.createRoot
+  const root = ReactDOM.createRoot(rootElement);
 
-    // Render the application
-    root.render(
-        <Provider store={store}>
-            {' '}
-            {/* Wrap App with Provider */}
-            <PersistGate loading={null} persistor={persistor}>
-                <Router basename="/team-project-03">
-                    {' '}
-                    {/* Wrap with Router and set basename */}
-                    <App />
-                </Router>
-            </PersistGate>
-        </Provider>,
-    );
+  // Render the application
+  root.render(
+    <Provider store={store}>
+      {" "}
+      {/* Wrap App with Provider */}
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          {" "}
+          {/* Wrap with Router and set basename */}
+          <App />
+        </Router>
+      </PersistGate>
+    </Provider>
+  );
 } else {
-    console.error('Root element not found');
+  console.error("Root element not found");
 }
