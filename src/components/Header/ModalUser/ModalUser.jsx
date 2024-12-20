ffb6a49e11064d19d3c27496593f2bb3041a6ae1
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-12-20 18:26:44 +0900

    update text

--- src/components/Header/ModalUser/ModalUser.jsx
+++ src/components/Header/ModalUser/ModalUser.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef } from 'react';
-import { IoSettingsOutline } from 'react-icons/io5';
-import { TbLogout } from 'react-icons/tb';
-import css from './ModalUser.module.css';
-import clsx from 'clsx';
+import { useEffect, useRef } from "react";
+import { IoSettingsOutline } from "react-icons/io5";
+import { TbLogout } from "react-icons/tb";
+import css from "./ModalUser.module.css";
+import clsx from "clsx";
 
 export default function ModalUser({
   contextModalIsOpen,
@@ -12,23 +12,23 @@ export default function ModalUser({
 }) {
   const modalRef = useRef(null);
   useEffect(() => {
-    const handleKeyDown = event => {
-      if (event.key === 'Escape') {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
         closeContext();
       }
     };
-    const handleMouseDown = event => {
+    const handleMouseDown = (event) => {
       if (modalRef.current && !modalRef.current.contains(event.target)) {
         closeContext();
       }
     };
     if (contextModalIsOpen) {
-      document.addEventListener('keydown', handleKeyDown);
-      document.addEventListener('mousedown', handleMouseDown);
+      document.addEventListener("keydown", handleKeyDown);
+      document.addEventListener("mousedown", handleMouseDown);
     }
     return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-      document.removeEventListener('mousedown', handleMouseDown);
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("mousedown", handleMouseDown);
     };
   }, [contextModalIsOpen, closeContext]);
   function handleSettingClick() {
@@ -41,22 +41,22 @@ export default function ModalUser({
   }
   return (
     <div
-      className={clsx(css['context-container'], {
-        [css['is-hidden']]: !contextModalIsOpen,
+      className={clsx(css["context-container"], {
+        [css["is-hidden"]]: !contextModalIsOpen,
       })}
       ref={modalRef}
     >
       <div className={css.context}>
         <button
-          className={css['context-button']}
+          className={css["context-button"]}
           onClick={handleSettingClick}
           type="button"
         >
           <IoSettingsOutline />
-          Setting
+          Settings
         </button>
         <button
-          className={css['context-button']}
+          className={css["context-button"]}
           type="button"
           onClick={handleLogOutClick}
         >
